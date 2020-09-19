3494ce157746e027a17c22d5401135aa0bcf0e1a
eltio <i.n.1998@inbox.ru>
2020-09-20 00:04:22 +0300

    Add log file support and minor changes
    PEP 8, remove commented lines, change project structure

--- bot.py
+++ bot.py
@@ -4,52 +4,64 @@ import time
 import urllib3
 import urllib3.request
 import tempfile
+import logging
+import sys
 
 import engine
 
-# You can leave this bit out if you're using a paid PythonAnywhere account
-proxy_url = "http://proxy.server:3128"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-}
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
-# end of the stuff that's only needed for free accounts
 
 def get_telegram_token():
     with open('token.txt', 'r') as f:
         return f.readline().strip()
 
-bot = telepot.Bot(get_telegram_token())
 
 def download_image(msg):
-    if len(msg['photo'])==0:
+    if len(msg['photo']) == 0:
         return ''
 
     file_name = msg['photo'][-1]['file_id']
-    path = tempfile.gettempdir()+'/' + file_name
+    path = tempfile.gettempdir() + '/' + file_name
     bot.download_file(file_name, path)
-    print(path)
     return path
 
 
 def handle(msg):
-    print(msg)
+    logging.info(msg)
     content_type, chat_type, chat_id = telepot.glance(msg)
 
     if content_type == 'text' and msg['text'] == '/start':
         bot.sendMessage(chat_id, "Hello! I'm @FaceDeleteBot. \n Send me photo and I detect and blur all faces on it.")
     elif content_type == 'photo':
         path = download_image(msg)
-        modified_path = engine.process_image(path)
-        with open(modified_path, 'rb') as modified_photo:
-            bot.sendPhoto(chat_id, modified_photo, reply_to_message_id=msg['message_id'])
+        output_path = engine.process_image(path)
+        logging.info('Input image: ' + path)
+        logging.info('Output image: ' + output_path)
+        with open(output_path, 'rb') as output_photo:
+            bot.sendPhoto(chat_id, output_photo, reply_to_message_id=msg['message_id'])
     else:
-        bot.sendMessage(chat_id, "Don't understand! Just send me photo and get result :)", reply_to_message_id=msg['message_id'])
+        bot.sendMessage(chat_id, "Don't understand! Just send me photo and get result :)",
+                        reply_to_message_id=msg['message_id'])
 
+
+def setup_pyanywhere_free_proxy():
+    """
+    Setup proxy for free PythonAnywhere account
+    """
+    proxy_url = "http://proxy.server:3128"
+    telepot.api._pools = {
+        'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+    }
+    telepot.api._onetime_pool_spec = (
+        urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30)
+    )
+
+
+setup_pyanywhere_free_proxy()
+logging.basicConfig(filename='facedeletebot.log', level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+bot = telepot.Bot(get_telegram_token())
 bot.message_loop(handle)
+print('Listening ...')
 
-print ('Listening ...')
-
-# Keep the program running.
 while 1:
     time.sleep(10)
--- engine.py
+++ engine.py
@@ -3,48 +3,50 @@ import cv2
 import dlib
 import numpy as np
 from imutils import face_utils
-   
+
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
+
+
 def detect_faces(image):
-    detect = dlib.get_frontal_face_detector()
-    predict = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
- 
     face_hulls = []
     subjects = detect(image, 0)
     for subject in subjects:
         shape = predict(image, subject)
-        shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+        shape = face_utils.shape_to_np(shape)
         hull = cv2.convexHull(shape)
         face_hulls.append(hull)
-    return face_hulls; 
-    
+    return face_hulls
+
+
 def blur_convex_hulls(image, hulls):
-    blurred_image = cv2.GaussianBlur(image,(93, 93), 20)
-   
-    mask = np.zeros(image.shape, np.uint8) 
+    blurred_image = cv2.GaussianBlur(image, (93, 93), 20)
+    mask = np.zeros(image.shape, np.uint8)
+
     for hull in hulls:
-        roi_corners = np.reshape(np.array(hull,dtype = np.int32), (1, 1, -1, 2))
-        cv2.fillPoly(mask, roi_corners, (255,)*image.shape[2])
-    mask_inverse = np.ones(mask.shape).astype(np.uint8)*255 - mask
-    
+        roi_corners = np.reshape(np.array(hull, dtype=np.int32), (1, 1, -1, 2))
+        cv2.fillPoly(mask, roi_corners, (255,) * image.shape[2])
+    mask_inverse = np.ones(mask.shape).astype(np.uint8) * 255 - mask
     return cv2.bitwise_and(blurred_image, mask) + cv2.bitwise_and(image, mask_inverse)
-    
+
 
 def process_image(path):
-    image = cv2.imread(path) 
-    #print('image size: {}'.format(image.shape))
-    
+    image = cv2.imread(path)
+
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = detect_faces(gray)
-    
     image = blur_convex_hulls(image, faces)
-    
-    #save_path = path[:path.rindex('.')] + '_out' + path[path.rindex('.'):]
-    save_path = path + '_out.jpg'
-    cv2.imwrite(save_path, image)
-    return save_path
+
+    try:
+        out_image_path = path[:path.rindex('.')] + '_out' + path[path.rindex('.'):]
+    except ValueError:
+        out_image_path = path + '.png'
+    cv2.imwrite(out_image_path, image)
+    return out_image_path
+
 
 if __name__ == '__main__':
     image_path = sys.argv[1]
-    modified_path = process_image(image_path)
-    cv2.imshow("image", cv2.imread(modified_path))
+    out_image_path = process_image(image_path)
+    cv2.imshow("image", cv2.imread(out_image_path))
     cv2.waitKey(0)
